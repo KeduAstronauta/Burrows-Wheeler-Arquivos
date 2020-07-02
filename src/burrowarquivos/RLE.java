a4cd176c9bf010b80970e1aa7d14fd876b1e0216
Fernando Pereira da Trindade <fernando.trindade1301@gmail.com>
2020-07-02 17:08:29 -0300

    Encode Decode

---
+++ src/burrowarquivos/RLE.java
@@ -0,0 +1,44 @@
+package burrowarquivos;
+
+public class RLE {
+	static String encode(String x) {
+		String y="";
+		char tmp=' ';
+		int cont=0;
+		for (int i = 0; i < x.length(); i++) {
+			tmp=x.charAt(i);
+			cont=1;
+			if(i==x.length()-1) {
+				y+=tmp+Integer.toString(cont);
+				break;
+			}
+			while(x.charAt(i+1)==tmp) {
+				cont++;
+				if(++i==x.length()-1) {
+					break;
+				}
+			}
+			y+=tmp+Integer.toString(cont);
+		}
+		return y;
+	}
+	
+	static String decode(String y) {
+		String x="";
+		int tmp;
+		for (int i = 0; i < y.length();) {
+			tmp=y.charAt(i+1)-'0';
+			while(tmp-->0) {
+				x+=y.charAt(i);
+			}
+			i=i+2;
+		}
+		return x;
+	}
+	public static void main(String[] args) {
+		String str=encode("teeste");
+		System.out.println(str);
+		String str1=decode(str);
+		System.out.println(str1);
+	}
+}
